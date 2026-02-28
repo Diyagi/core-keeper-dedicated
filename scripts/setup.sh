34f1866753e0a5aea0a9e2bc4fbedf2328f21800
Diyagi <github@diyagi.dev>
2026-02-28 20:50:35 -0300

    Fix existing game install not updating

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,33 +4,53 @@ source "${SCRIPTSDIR}/mod-manager.sh"
 
 mkdir -p "${STEAMAPPDIR}" || true
 
-# Initialize arguments array
-args=(
-    "+@sSteamCmdForcePlatformType" "linux"
-    "+@sSteamCmdForcePlatformBitness" "64"
-    "+force_install_dir" "$STEAMAPPDIR"
-    "+login" "anonymous"
-    "+app_update" "$STEAMAPPID" "validate"
-    "+app_update" "$STEAMAPPID_TOOL" "validate"
-)
+NEEDS_VALIDATION=false
 
-# Override SteamCMD launch arguments if necessary
-# Used for subscribing to betas or for testing
-if [ -n "$STEAMCMD_UPDATE_ARGS" ]; then
-    args+=("${STEAMCMD_UPDATE_ARGS[@]}")
+if [ ! -f "${STEAMAPPDIR}/steamapps/appmanifest_1963720.acf" ]; then
+    NEEDS_VALIDATION=true
 fi
 
-# Add the quit command
-args+=("+quit")
-
-# Run SteamCMD with the arguments
 if [ "${USE_DEPOT_DOWNLOADER}" == true ]; then
-    DepotDownloader -app $STEAMAPPID -osarch 64 -dir $STEAMAPPDIR -validate
-    DepotDownloader -app $STEAMAPPID_TOOL -osarch 64 -dir $STEAMAPPDIR -validate
-    chmod +x $STEAMAPPDIR/CoreKeeperServer
+    app_args=("-app" "$STEAMAPPID" "-osarch" "64" "-dir" "$STEAMAPPDIR")
+    tool_args=("-app" "$STEAMAPPID_TOOL" "-osarch" "64" "-dir" "$STEAMAPPDIR")
+
+    if [ "$NEEDS_VALIDATION" == true ]; then
+        app_args+=("-validate")
+        tool_args+=("-validate")
+    fi
+
+    DepotDownloader "${app_args[@]}"
+    DepotDownloader "${tool_args[@]}"
+    
 else
+    args=(
+        "+@sSteamCmdForcePlatformType" "linux"
+        "+@sSteamCmdForcePlatformBitness" "64"
+        "+force_install_dir" "$STEAMAPPDIR"
+        "+login" "anonymous"
+    )
+
+    app_args=("+app_update" "$STEAMAPPID")
+    tool_args=("+app_update" "$STEAMAPPID_TOOL")
+
+    if [ "$NEEDS_VALIDATION" == true ]; then
+        app_args+=("validate")
+        tool_args+=("validate")
+    fi
+
+    args+=("${app_args[@]}")
+    args+=("${tool_args[@]}")
+
+    if [ -n "$STEAMCMD_UPDATE_ARGS" ]; then
+        args+=("${STEAMCMD_UPDATE_ARGS[@]}")
+    fi
+
+    args+=("+quit")
+
     "$STEAMCMDDIR/steamcmd.sh" "${args[@]}"
 fi
+
+chmod +x "$STEAMAPPDIR/CoreKeeperServer"
 
 manage_mods
 
